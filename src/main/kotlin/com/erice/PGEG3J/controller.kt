817586970129101b54b8dcc0cc52eb46f9580ae7
Eric Engelhart <bob80333@gmail.com>
2018-01-07 02:01:44 -0500

    Remove unused code

--- src/main/kotlin/com/erice/PGEG3J/controller.kt
+++ src/main/kotlin/com/erice/PGEG3J/controller.kt
@@ -10,31 +10,4 @@ import java.nio.charset.Charset
  */
 class MapController : Controller() {
 
-    /**
-     * random quotes from resource quotes.txt
-     */
-    val quotes = File(javaClass.classLoader.getResource("quotes.txt").toURI()).readLines(Charset.forName("UTF-8"))
-
-    /**
-     * the list of open text editors
-     */
-    val editorModelList = mutableListOf<TextEditorFragment>().observable()
-
-    fun newEditor(): TextEditorFragment {
-        val newFile = DocumentViewModel()
-        newFile.title.value = "New file ${editorModelList.size}"
-        newFile.commit()
-
-        val editor = TextEditorFragment(newFile)
-        editorModelList.add(editor)
-
-        return editor
-    }
-
-    /**
-     * provides a random quote
-     */
-    fun quote(): String = quotes[(Math.random() * quotes.size).toInt()]
-
-
 }
